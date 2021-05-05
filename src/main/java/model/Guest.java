4a6ca661f0b10894a8eafe8e3ad457a1378a4887
Paula <rusti.paula78@gmail.com>
2021-05-05 15:35:49 +0300

    Created a temporary class for writing Users in json file
    Added maven dependency for jackson
    Modified module-info to make it work
    Created the FileWriter &  FilePath classes

--- src/main/java/model/Guest.java
+++ src/main/java/model/Guest.java
@@ -3,9 +3,9 @@ package model;
 public class Guest extends User
 {
     public Guest(){}
-    public Guest(String username, String name, String surname, String tel, String email, String password, String role)
+    public Guest(String username, String name, String surname, String tel, String email, String password)
     {
-        super(username, name, surname, tel, email, password, role);
+        super(username, name, surname, tel, email, password, "guest");
     }
 
 }
--- src/main/java/model/Organizer.java
+++ src/main/java/model/Organizer.java
@@ -4,9 +4,9 @@ public class Organizer extends User
 {
     public Organizer(){ }
 
-    public Organizer(String username, String name, String surname, String tel, String email, String password, String role)
+    public Organizer(String username, String name, String surname, String tel, String email, String password)
     {
-        super(username, name, surname, tel, email, password, role);
+        super(username, name, surname, tel, email, password, "organizer");
     }
 }
 
